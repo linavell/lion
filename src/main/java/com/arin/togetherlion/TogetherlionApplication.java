bee9ae7be0a9ee416ff0a03e5b306ea669ba0760
linavell <linavell@naver.com>
2024-06-15 15:37:05 +0900

    init commit

---
+++ src/main/java/com/arin/togetherlion/TogetherlionApplication.java
@@ -0,0 +1,13 @@
+package com.arin.togetherlion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherlionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherlionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arin/togetherlion/TogetherlionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arin.togetherlion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherlionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
